c709f75b71b5dbccf1c0c2c9b968297b87b6b260
thissidemayur <thissidemayur@gmail.com>
2025-12-25 12:31:45 +0530

    backend: rewrite factors api

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,6 +4,7 @@ import { Router } from "express";
 import { authRouter } from "./auth.route.js";
 import { userRouter } from "./user.route.js";
 import { footprintRouter } from "./footprint.routes.js";
+import {factorRouter} from "./emmisionFactor.route.js"
 import {adminRouter} from "./admin.route.js"
 import { healthCheckRouter } from "./healthCheck.js";
 const router = Router();
@@ -24,10 +25,10 @@ router.use("/footprints", footprintRouter);
 // Base URL: /api/v1/admin
 router.use("/admin", adminRouter);
 
-
+// --- Emission Factor Management Routes ---
+// Base URL: /api/v1/factors
+router.use("/factors", factorRouter);
 
 
 // --- Health Check Route ---
-// Base URL: /api/v1/health
-router.use(healthCheckRouter)
 export default router;
